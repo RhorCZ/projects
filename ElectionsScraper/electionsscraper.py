3d5026e62bf78a901018977fbfc246c1d2801ce2
RhorCZ <horvath.r@email.cz>
2021-12-31 23:30:45 +0100

    Improved readability

--- ElectionsScraper/electionsscraper.py
+++ ElectionsScraper/electionsscraper.py
@@ -6,23 +6,8 @@ from typing import Union
 from bs4 import BeautifulSoup
 
 
+# Process given arguments
 def get_arguments():
-    # Make sure that filename is original
-    def process_filename(filename: str) -> str:
-        new_filename = filename
-        path, file = os.path.split(filename)
-        original_filename = file
-        index = 1
-        while True:
-            if os.path.exists(os.path.join(path, file)):
-                # Rename
-                file = original_filename.split(".")[0] + "(" + str(index) + ")" + ".csv"
-                index += 1
-                new_filename = os.path.join(path, file)
-            else:
-                break
-        return new_filename
-
     try:
         if not len(sys.argv) == 3:
             raise Exception("Wrong number of arguments! Expected two of them!")
@@ -30,11 +15,28 @@ def get_arguments():
             raise Exception("Invalid URL!")
         elif len(sys.argv[2]) <= 4 or not sys.argv[2].endswith(".csv"):
             raise Exception("Invalid filename!")
-        return sys.argv[1], process_filename(sys.argv[2])
+        return sys.argv[1], sys.argv[2]
     except Exception as ex:
         print(ex)
         print('Example: python electionsscraper.py "elections-url" "filename.csv"')
         exit(0)
+
+
+# Make sure that filename is original
+def process_filename(filename: str) -> str:
+    new_filename = filename
+    path, file = os.path.split(filename)
+    original_filename = file
+    index = 1
+    while True:
+        if os.path.exists(os.path.join(path, file)):
+            # Rename
+            file = original_filename.split(".")[0] + "(" + str(index) + ")" + ".csv"
+            index += 1
+            new_filename = os.path.join(path, file)
+        else:
+            break
+    return new_filename
 
 
 def get_villages(url: str) -> dict():
@@ -44,7 +46,7 @@ def get_villages(url: str) -> dict():
         soup = BeautifulSoup(page.text, "html.parser")
         rows = soup.find_all('tr')
 
-        def add_village(number, name, link):
+        def add_village(number: str, name: str, link):
             if link is None:
                 return
             village_link = link.get("href")
@@ -58,11 +60,11 @@ def get_villages(url: str) -> dict():
             if len(columns) >= 3:
                 # Get correct link from new/old elections tables
                 if len(columns) == 3 or len(columns) == 7:
-                    add_village(columns[0].text, columns[1].text, columns[0].find('a'))
+                    add_village(columns[0].text.strip(), columns[1].text.strip(), columns[0].find('a'))
                     if len(columns) == 7:
-                        add_village(columns[4].text, columns[5].text, columns[4].find('a'))
-                elif len(columns) == 4:
-                    add_village(columns[0].text, columns[1].text, columns[2].find('a'))
+                        add_village(columns[4].text.strip(), columns[5].text.strip(), columns[4].find('a'))
+                else:
+                    add_village(columns[0].text.strip(), columns[1].text.strip(), columns[2].find('a'))
 
         if village_dict is None:
             raise Exception("Failed to get villages from URL!")
@@ -77,43 +79,6 @@ def get_villages(url: str) -> dict():
 def get_election_data(villages: dict, header: list) -> Union[list, list]:
     csv_rows = list()
     csv_header = header
-
-    def process_header_table(rows):
-        csv_row = list()
-        for row in rows:
-            columns = row.find_all("td")
-            if columns is not None and len(columns) >= 7:
-                csv_row = [columns[3].text, columns[4].text, columns[7].text]
-                break
-        return csv_row
-
-    def process_main_table(rows):
-        csv_row = list()
-        candidates = list()
-        for row in rows:
-            columns = row.find_all("td")
-            if columns is not None:
-                # Handle newer elections result table
-                if len(columns) == 5:
-                    candidate_name = columns[1].text.strip()
-                    candidate_result = columns[2].text.strip()
-                    if not candidate_name == "-":
-                        csv_row.append(candidate_result)
-                        candidates.append(candidate_name)
-                # Handle older elections result table
-                elif len(columns) == 9:
-                    candidate_name = columns[1].text.strip()
-                    candidate_name2 = columns[6].text.strip()
-                    candidate_result = columns[2].text.strip()
-                    candidate_result2 = columns[7].text.strip()
-                    if not candidate_name == "":
-                        csv_row.append(candidate_result)
-                        candidates.append(candidate_name)
-                    if not candidate_name2 == "":
-                        csv_row.append(candidate_result2)
-                        candidates.append(candidate_name2)
-
-        return csv_row, candidates
 
     for key, value in villages.items():
         try:
@@ -147,19 +112,61 @@ def get_election_data(villages: dict, header: list) -> Union[list, list]:
     return csv_rows, csv_header
 
 
+# Part of get_election_data
+def process_header_table(rows):
+    csv_row = list()
+    for row in rows:
+        columns = row.find_all("td")
+        if columns is not None and len(columns) >= 7:
+            csv_row = [columns[3].text.strip(), columns[4].text.strip(), columns[7].text.strip()]
+            break
+    return csv_row
+
+
+# Part of get_election_data
+def process_main_table(rows):
+    csv_row = list()
+    candidates = list()
+    for row in rows:
+        columns = row.find_all("td")
+        if columns is not None:
+            # Handle newer elections result table
+            if len(columns) == 5:
+                candidate_name = columns[1].text.strip()
+                candidate_result = columns[2].text.strip()
+                if not candidate_name == "-":
+                    csv_row.append(candidate_result)
+                    candidates.append(candidate_name)
+            # Handle older elections result table
+            elif len(columns) == 9:
+                candidate_name = columns[1].text.strip()
+                candidate_name2 = columns[6].text.strip()
+                candidate_result = columns[2].text.strip()
+                candidate_result2 = columns[7].text.strip()
+                if not candidate_name == "":
+                    csv_row.append(candidate_result)
+                    candidates.append(candidate_name)
+                if not candidate_name2 == "":
+                    csv_row.append(candidate_result2)
+                    candidates.append(candidate_name2)
+
+    return csv_row, candidates
+
+
 def write_csv(file: str, header: list, rows: list):
     try:
         with open(file, "w", encoding="utf-8", newline='') as f:
             writer = csv.writer(f)
             writer.writerow(header)
             writer.writerows(rows)
-    except:
+    except Exception:
         print("Error occured:\n", sys.exc_info()[0])
         exit(0)
 
 
 def main():
     url, file = get_arguments()
+    file = process_filename(file)
     villages = get_villages(url)
     initial_header = ["Kod obce", "Nazev obce", "Volici v seznamu", "Vydane obalky", "Platne hlasy"]
     csv_rows, csv_header = get_election_data(villages, initial_header)
